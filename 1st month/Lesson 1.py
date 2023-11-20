8c613bfbc373fa8f5b082c2e34a14887ce4209dd
bekturtoktobekov <149789887+bekturtoktobekov@users.noreply.github.com>
2023-11-20 16:39:36 +0600

    Delete 1st month directory

--- 1st month/Lesson 1.py
+++
@@ -1,7 +0,0 @@
-# name = input('Enter your name:')
-# age = int(input('Enter your age:'))
-# current_year = 2023
-# born_year = (current_year - age)
-#
-# print(f'NAME: {name} AGE: {age} BORN: {born_year}')
-
